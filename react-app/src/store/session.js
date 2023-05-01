fd2d9138beba62eb44a6cb01563582e142422844
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-05-01 10:08:05 -0700

    fix user signup

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -67,7 +67,7 @@ export const logout = () => async (dispatch) => {
 	}
 };
 
-export const signUp = (username, email, password) => async (dispatch) => {
+export const signUp = (username, email, first_name,last_name,password) => async (dispatch) => {
 	const response = await fetch("/api/auth/signup", {
 		method: "POST",
 		headers: {
@@ -76,6 +76,8 @@ export const signUp = (username, email, password) => async (dispatch) => {
 		body: JSON.stringify({
 			username,
 			email,
+			first_name,
+			last_name,
 			password,
 		}),
 	});
