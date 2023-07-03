ef5603f56e2580a1ef06c6c9b1b70a8937b45b1c
alex-rafael-13 <rafael.alex1398@gmail.com>
2023-07-03 16:01:28 -0700

    added comments table

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -7,4 +7,6 @@ from .product_categories import Category
 from .product_images import Product_Image
 from .product_conditions import Product_Condition
 from .event_type import Event_Type
+from .event_comments import Event_Comments
 from .db import environment, SCHEMA
+
